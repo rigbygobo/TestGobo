fd2b3beb70ae64681cea20742d35a299b55d51f1
jamesrigby <>
2016-04-15 14:02:25 +0100

    Ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Ball start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
